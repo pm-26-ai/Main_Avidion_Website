ff64bce95aef20bbdd9c54c6e97ce9c9d89bbbe8
pm-26-ai <projectmanager@avidion.ai>
2025-05-07 17:00:43 +0530

    add : floatimg-whatsapp-icon

---
+++ src/app/careers/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Careers",
+  description:
+    "Explore career opportunities at Avidion. Join a team of innovators building cutting-edge web, mobile, and AI solutions. Discover roles that match your passion and skills.",
+};
+
+export default function CareersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client";
 import ContactForm from "@/components/contact-form";
-
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 export default function HomePage() {
   return (
     <>
+    <div className="text-pri">Heloo</div>
       <ContactForm />
+      <FloatingWhatsApp
+        notification
+        notificationSound
+        phoneNumber="9896584099"
+        accountName="Enquiry"
+        avatar="./avidion-logo.png"
+      />
     </>
   );
 }
